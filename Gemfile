b62d68855cf53be3d36ddd01dffce862cf3500fa
Daniel Bryant <bryant.daniel.j@gmail.com>
2015-02-21 22:50:25 -0500

    add redcarpet gem and a description to users

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'haml-rails', '~> 0.8'
 gem 'font-awesome-sass', '~> 4.3.0'
 gem 'bootstrap-sass', '~> 3.3.3'
 gem 'bcrypt', '~> 3.1.10'
+gem 'redcarpet', '~> 3.2.2'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:name, :email, :title, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :title, :description, :password, :password_confirmation)
     end
 
     def logged_in_user
