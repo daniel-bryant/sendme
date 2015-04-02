3ceed0f6911107bbcc9393ba256eab20c2a252ed
Daniel Bryant <bryant.daniel.j@gmail.com>
2015-04-02 00:00:33 -0400

    render users/create when a user creates an account instead of redirecting to root

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,6 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       UserMailer.account_activation(@user).deliver_now
-      flash[:info] = "Please check your email to activate your account."
-      redirect_to root_url
     else
       render :new
     end
