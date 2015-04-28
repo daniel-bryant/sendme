9e9503ef56b13ccb91bc6e0d237c4f18bf8e1636
Daniel Bryant <bryant.daniel.j@gmail.com>
2015-04-28 14:44:44 -0400

    fix cursor movements between para's

--- app/assets/javascripts/documents.js
+++ app/assets/javascripts/documents.js
@@ -265,7 +265,7 @@ $(document).on('page:change', function() {
       init_cursor($paragraphs.first().contents().first());
     } else {
       var $new_para = $('<div class="para" style="text-align: left; line-height: 1;"></div>');
-      var $nbsp_char = $('<span class="nbsp-char" style="font-family:Arial;font-size:24px;font-weight:normal;font-style:normal;text-decoration:none;">&nbsp;</span>');
+      var $nbsp_char = $('<span class="char nbsp-char" style="font-family:Arial;font-size:24px;font-weight:normal;font-style:normal;text-decoration:none;">&nbsp;</span>');
       $('#page1').prepend($new_para);
       $new_para.prepend($nbsp_char);
       init_cursor($nbsp_char);
