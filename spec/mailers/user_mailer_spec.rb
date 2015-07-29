defc814678a8bbb8cff040440669f41036d6871f
Daniel Bryant <bryant.daniel.j@gmail.com>
2015-07-29 00:09:51 -0400

    fix UserMailer specs

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,31 +1,36 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, :type => :mailer do
+  let(:user) { FactoryGirl.create(:user) }
+
   describe "account_activation" do
-    let(:mail) { UserMailer.account_activation }
+    let(:mail) { UserMailer.account_activation(user) }
 
     it "renders the headers" do
       expect(mail.subject).to eq("Account activation")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["donotreply@sendmeacopy.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      activation_url = edit_activation_url(user.activation_token, email: user.email)
+      expect(mail.body.encoded).to include(activation_url)
     end
   end
 
   describe "password_reset" do
-    let(:mail) { UserMailer.password_reset }
+    let(:mail) { UserMailer.password_reset(user) }
+    before { user.create_reset_digest }
 
     it "renders the headers" do
       expect(mail.subject).to eq("Password reset")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["donotreply@sendmeacopy.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      password_reset_url = edit_password_reset_url(user.reset_token, email: user.email)
+      expect(mail.body.encoded).to include(password_reset_url)
     end
   end
 
