6d45d266fa5b22b9313a87ac638f178d0eae3f03
Daniel Bryant <bryant.daniel.j@gmail.com>
2015-07-30 23:09:14 -0400

    require admin for entire posts controller, add 'beta' warning

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :logged_in_user, only: [:new, :create, :edit, :update]
-  before_action :admin_user,     only: [:new, :create, :edit, :update]
+  before_action :admin_user #,     only: [:new, :create, :edit, :update]
 
   def index
     @posts = Post.all
